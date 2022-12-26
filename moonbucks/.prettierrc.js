c1b50098f66328c409d5710108e9b00813176d0d
kyejin0412 <kyejin0412@kookmin.ac.kr>
2022-12-26 15:01:13 +0900

    create project

---
+++ moonbucks/.prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  //줄 바꿈 폭 길이
+  "printWidth": 100,
+  //탭 너비
+  "tabWidth": 2,
+  //탭 사용 여부
+  "useTabs": false,
+  //세미클론 여부
+  "semi": true,
+  //' or "
+  "singleQuote": true,
+  //여러줄 사용시, 뒷줄 콤마 여부
+  "trailingComma": "all"
+}
