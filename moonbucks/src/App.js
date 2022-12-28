54c61a26621b5e7fa7eedbd3f1705910c4494b05
kyejin0412 <kyejin0412@kookmin.ac.kr>
2022-12-29 00:36:17 +0900

    FIX : App.js 라우터 연결

--- moonbucks/src/App.js
+++ moonbucks/src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 
 const App = () => {
     return (
-      <Home/>
+      <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Home/>} />
+        </Routes>
+      </BrowserRouter>
     );
   };
 
